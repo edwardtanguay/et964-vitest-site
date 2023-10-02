fa19c9c9c0c2b73164912659f4a974f46a1a8dd8
Edward Tanguay (windows) <edward@tanguay.info>
2023-10-02 02:48:01 +0200

    first function tested

--- src/tools/qstr.test.ts
+++ src/tools/qstr.test.ts
@@ -3,5 +3,21 @@ import { capitalizeFirstLetter } from "./qstr";
 describe('capitalizeFirstLetter', () => {
 	it('uppercases and lowercases correct letters', () => {
 		expect(capitalizeFirstLetter('test')).toBe('Test');
-	})
+		expect(capitalizeFirstLetter('Test')).toBe('Test');
+		expect(capitalizeFirstLetter('TEST')).toBe('Test');
+		expect(capitalizeFirstLetter('two words')).toBe('Two words');
+	});
+	it('handles all edge cases', () => {
+		expect(capitalizeFirstLetter('')).toBe('');
+		expect(capitalizeFirstLetter('  ')).toBe('  ');
+		expect(capitalizeFirstLetter('!@#$%^&*()')).toBe('!@#$%^&*()');
+	});
+	it('handles German characters', () => {
+		expect(capitalizeFirstLetter('äpfel')).toBe('Äpfel');
+		expect(capitalizeFirstLetter('öfter')).toBe('Öfter');
+		expect(capitalizeFirstLetter('über')).toBe('Über');
+		expect(capitalizeFirstLetter('ÄÄÄÄ')).toBe('Ääää');
+		expect(capitalizeFirstLetter('ÖÖÖÖ')).toBe('Öööö');
+		expect(capitalizeFirstLetter('ÜÜÜÜ')).toBe('Üüüü');
+	});
 });

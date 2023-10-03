f76e7a26385a66bd7bd8235f555cdd987b06e782
Edward Tanguay (windows) <edward@tanguay.info>
2023-10-03 17:36:25 +0200

    coverage

--- src/tools/qstr.ts
+++ src/tools/qstr.ts
@@ -8,3 +8,7 @@
 export const capitalizeFirstLetter = (text: string) => {
 	return text.charAt(0).toUpperCase() + text.slice(1).toLowerCase();
 }
+
+export const sentencize = (text: string) => {
+	return 'nnn';
+}
